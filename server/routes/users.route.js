200472e1d49143bfac555accddd0befa1120c3b8
Haris Khan <haris.a.khan@stonybrook.edu>
2022-07-26 01:12:06 -0400

    Finish routes

--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -1,6 +1,15 @@
 const express = requre("express");
 const router = express.Router();
-router.get('/',(res,req)=>{
-    
+let UserModel = require("./../models/user.model.js");
+router.route("/").get((req,res)=>{
+    UserModel.find()
+    .then((users)=>res.json(users))
+    .catch((err)=>res.status(400).json(err))
 });
-export default router
+router.route("/add"),post((req,res)=>{
+    const username = req.body.username;
+    const CreatedUser = new UserModel((username));
+    CreatedUser.save()
+    .then(()=>res.json("New User added")).catch((err)=>res.status(400).json(err))
+});
+export default router;
